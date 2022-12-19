779c3b30c15c27a0afec16b0d1a8de1ea05b08ba
James Addison <71357672+siliconjungle@users.noreply.github.com>
2022-12-19 12:31:58 +1100

    Update index.js

--- index.js
+++ index.js
@@ -1,33 +1,33 @@
-const createVector3 = (x, y, z) => ({ x, y, z })
+export const create = (x, y, z) => ({ x, y, z })
 
-const length = (data) => Math.sqrt(data.x * data.x + data.y * data.y + data.z * data.z)
+export const length = (data) => Math.sqrt(data.x * data.x + data.y * data.y + data.z * data.z)
 
-const normalize = (data) => {
+export const normalize = (data) => {
   const len = length(data)
   return len > 0
-    ? createVector3(data.x / len, data.y / len, data.z / len)
-    : createVector3(data.x, data.y, data.z)
+    ? create(data.x / len, data.y / len, data.z / len)
+    : create(data.x, data.y, data.z)
 }
 
-const add = (data, other) => createVector3(data.x + other.x, data.y + other.y, data.z + other.z)
+export const add = (data, other) => create(data.x + other.x, data.y + other.y, data.z + other.z)
 
-const subtract = (data, other) => createVector3(data.x - other.x, data.y - other.y, data.z - other.z)
+export const subtract = (data, other) => create(data.x - other.x, data.y - other.y, data.z - other.z)
 
-const multiply = (data, scalar) => createVector3(data.x * scalar, data.y * scalar, data.z * scalar)
+export const multiply = (data, scalar) => create(data.x * scalar, data.y * scalar, data.z * scalar)
 
-const divide = (data, scalar) => createVector3(data.x / scalar, data.y / scalar, data.z / scalar)
+export const divide = (data, scalar) => create(data.x / scalar, data.y / scalar, data.z / scalar)
 
-const dot = (data, other) => data.x * other.x + data.y * other.y + data.z * other.z
+export const dot = (data, other) => data.x * other.x + data.y * other.y + data.z * other.z
 
-const cross = (data, other) =>
-  createVector3(data.y * other.z - data.z * other.y, data.z * other.x - data.x * other.z, data.x * other.y - data.y * other.x)
+export const cross = (data, other) =>
+  create(data.y * other.z - data.z * other.y, data.z * other.x - data.x * other.z, data.x * other.y - data.y * other.x)
 
-const v = createVector3(1, 2, 3)
+const v = create(1, 2, 3)
 console.log(length(v))
 const v2 = normalize(v)
-const v3 = add(v2, createVector3(2, 3, 4))
-const v4 = subtract(v2, createVector3(2, 3, 4))
+const v3 = add(v2, create(2, 3, 4))
+const v4 = subtract(v2, create(2, 3, 4))
 const v5 = multiply(v2, 2)
 const v6 = divide(v2, 2)
-const v7 = dot(v2, createVector3(2, 3, 4))
-const v8 = cross(v2, createVector3(2, 3, 4))
+const v7 = dot(v2, create(2, 3, 4))
+const v8 = cross(v2, create(2, 3, 4))
